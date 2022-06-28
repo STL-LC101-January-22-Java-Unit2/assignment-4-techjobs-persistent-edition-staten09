94c5373ee8b6a52e04e9d67d66b2630d7d05c176
staten09 <staten09@gmail.com>
2022-06-27 19:26:05 -0500

    completed assignment 4

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,40 @@
 package org.launchcode.techjobs.persistent.models;
 
+import org.hibernate.internal.JdbcObserverImpl;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank
+    @Size(max = 250)
+    private String description;
+
+    @NotBlank
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<>();
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
+
+    public Skill(){}
 }
